e59f3de3b12fa090348d42bf697cb1925a4ffdc6
Hadrian Hughes <h.hughes@amaze.com>
2018-07-10 22:59:38 +0100

    Add breakpoints mixin and create grid system

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,6 +5,7 @@ import Helmet from 'react-helmet'
 import Header from '../components/header'
 import './index.css'
 import '../styles/global.scss';
+import '../styles/grid.scss';
 
 const Layout = ({ children, data }) => (
   <div>
@@ -16,15 +17,10 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+    <div className="row">
+      <div className="column medium-12">
+        {children()}
+      </div>
     </div>
   </div>
 )
