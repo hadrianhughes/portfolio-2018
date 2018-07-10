10139aae021ae2ae63dfa9f7e36a307d98a981ea
Hadrian Hughes <h.hughes@amaze.com>
2018-07-10 23:19:34 +0100

    Move contents of index.css to global.scss

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
 import Header from '../components/header';
-import './index.css';
 import '../styles/global.scss';
 import '../styles/grid.scss';
 
